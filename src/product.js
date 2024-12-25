47a493490ce7a2e4fc4c650c0180deb00e460d85
Gray <blackcat5523@gmail.com>
2024-12-25 13:41:25 +0800

    提交一部分二

---
+++ src/product.js
@@ -0,0 +1,58 @@
+import product1 from "./assets/1.png";
+import product2 from "./assets/2.png";
+import product3 from "./assets/3.png";
+import product4 from "./assets/4.png";
+import product5 from "./assets/5.png";
+import product6 from "./assets/6.png";
+import product7 from "./assets/7.png";
+import product8 from "./assets/8.png";
+
+export const PRODUCTS = [
+    {
+        id:1,
+        productName:"Black Tea",
+        price:35,
+        productImage:product1,
+    },
+    {
+        id:2,
+        productName:"Green Tea",
+        price:35,
+        productImage:product2,
+    },
+    {
+        id:3,
+        productName:"Mesona Tea",
+        price:40,
+        productImage:product3,
+    },
+    {
+        id:4,
+        productName:"Mike Tea",
+        price:55,
+        productImage:product4,
+    },
+    {
+        id:5,
+        productName:"Pearl Milk Tea",
+        price:55,
+        productImage:product5,
+    },
+    {
+        id:6,
+        productName:"Guava Lemon Juice",
+        price:70,
+        productImage:product6,
+    },
+    {
+        id:7,
+        productName:"Orange Green Tea",
+        price:75,
+        productImage:product7,
+    },{
+        id:8,
+        productName:"Tomato Plum Juice",
+        price:70,
+        productImage:product8,
+    }
+]
